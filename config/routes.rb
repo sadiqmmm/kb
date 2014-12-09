23cb2fd46f7bc8c282643a7db8c73d916307d84f
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-12-09 18:44:52 +0530

    Run Pending migrations, add hover on book edit coffie, change brand name, Fix the root route to book index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :books
-
-  devise_for :users  root to: "topics#index"
+  
+  devise_for :users  
+  root to: "books#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
