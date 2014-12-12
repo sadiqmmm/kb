fc6c3f2a17096a95e510b0c146de746a150b8c7b
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-12-12 23:28:10 +0530

    Add ngprogress and configure it

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,7 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require nprogress
+//= require nprogress-turbolinks
 //= require turbolinks
 //= require_tree .
